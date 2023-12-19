3521259d362e4b08532dd35cfb461e8c11abe0cf
romain dusson <romain.dusson@alterway.fr>
2023-12-19 09:20:31 +0100

    add package install

--- setup.sh
+++ setup.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
-set -euo pipefail
-
 #########################################
 ### Script prepare linux workstation ####
 #########################################
 
+set -euo pipefail
+#exec 2>/dev/null
 
 install_app() {
-  pkg_list="git tmux neovim npm firefox htop"
+  pkg_list="git tmux npm firefox htop flatpak"
 
-  if [[ $(cat /etc/*-release) == "*debian*" || $(cat /etc/*-release) == "*ubuntu*" ]]; then
+  if [[ $(cat /etc/*-release) == *"debian"* || $(cat /etc/*-release) == *"ubuntu"* ]]; then
     echo "os debian or ubuntu like"
-  elif [[ $(cat /etc/*-release) == "*arch*" ]]; then
+    apt update && apt upgrade
+    apt install -y "$pkg_list"
+  elif [[ $(cat /etc/*-release) == *"arch"* ]]; then
     echo "os archlinux"
+    pacman -Syu --noconfirm
+    pacman -S flatpak --noconfirm
   else
     echo "os not supported by script"
   fi
 }
+
+install_app
