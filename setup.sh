1b4d03e70c21eb8a30550956e41e63ae8b5686b3
romain dusson <romain.dusson@alterway.fr>
2023-12-19 16:03:27 +0100

    add multiple crazy shit

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ set -euo pipefail
 #exec 2>/dev/null
 
 install_app() {
-  pkg_list="git tmux npm firefox htop flatpak"
+  pkg_list="git tmux npm firefox htop flatpak fish ripgrep gcc"
 
   if [[ $(cat /etc/*-release) == *"debian"* || $(cat /etc/*-release) == *"ubuntu"* ]]; then
     echo "os debian or ubuntu like"
@@ -23,6 +23,30 @@ install_app() {
   fi
 
   flatpak install flathub io.neovim.nvim
+  curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+  curl -sS https://starship.rs/install.sh | sh
 }
 
-install_app
+configure() {
+  mkdir $HOME/.themes
+  mkdir $HOME/.fonts
+  mkdir $HOME/.config
+  mkdir $HOME/.config/fish
+
+  chsh -s $(which fish)
+
+  wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip $HOME
+  unzip $HOME/JetBrainsMono.zip -d $HOME/.fonts 
+
+  git clone git@github.com:n0xas69/dotfile.git $HOME
+  git clone git@github.com:n0xas69/nvchad_config.git $HOME
+  git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+  mkdir $HOME/.config/nvim/lua/custom
+  git clone git@github.com:n0xas69/nvchad_config.git ~/.config/nvim/lua/custom
+
+  cp $HOME/dotfile/.tmux.conf $HOME/.tmux.conf
+  cp $HOME/dotfile/.config/fish/config.fish $HOME/.config/fish/config.fish
+  cp $HOME/dotfile/.config/kitty/kitty.conf $HOME/.config/kitty/kitty.conf
+  cp $HOME/dotfile/.config/starship.toml $HOME/.config/starship.toml
+
+}
