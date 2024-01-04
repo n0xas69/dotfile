5f48f3aff503969550f474669250395411ea2595
n0xas <dusson.romain@proton.me>
2024-01-04 22:47:05 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 ### Script prepare linux workstation ####
 #########################################
 
-set -euo pipefail
+#set -euo pipefail
 #exec 2>/dev/null
 
 install_app() {
@@ -16,8 +16,8 @@ install_app() {
     apt install -y "$pkg_list"
   elif [[ $(cat /etc/*-release) == *"arch"* ]]; then
     echo "os archlinux"
-    pacman -Syu --noconfirm
-    pacman -S flatpak --noconfirm
+    sudo pacman -Syu --noconfirm
+    sudo pacman -S flatpak --noconfirm
   else
     echo "os not supported by script"
   fi
@@ -36,10 +36,13 @@ configure() {
   chsh -s $(which fish)
 
   wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip $HOME
-  unzip $HOME/JetBrainsMono.zip -d $HOME/.fonts 
+  unzip $HOME/JetBrainsMono.zip -d $HOME/.fonts
 
-  git clone git@github.com:n0xas69/dotfile.git $HOME
-  git clone git@github.com:n0xas69/nvchad_config.git $HOME
+  cd $HOME
+  git clone git@github.com:n0xas69/dotfile.git
+  git clone git@github.com:n0xas69/nvchad_config.git
+  rm -rf ~/.config/nvim
+  rm -rf ~/.local/share/nvim
   git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
   mkdir $HOME/.config/nvim/lua/custom
   git clone git@github.com:n0xas69/nvchad_config.git ~/.config/nvim/lua/custom
@@ -50,3 +53,6 @@ configure() {
   cp $HOME/dotfile/.config/starship.toml $HOME/.config/starship.toml
 
 }
+
+install_app
+configure
