6ffe93db4d0098b0f957c6faa020da94a35064eb
n0xas <dusson.romain@proton.me>
2024-04-20 13:41:23 +0200

    add lazygit

--- setup.sh
+++ setup.sh
@@ -42,6 +42,13 @@ install_app() {
   sudo mv $HOME/zellij /usr/local/bin/
   rm $HOME/zellij-x86_64-unknown-linux-musl.tar.gz
 
+  # install lazygit
+  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+  curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+  tar xf lazygit.tar.gz lazygit
+  sudo install lazygit /usr/local/bin
+  rm lazygit.tar.gz
+
 }
 
 configure() {
