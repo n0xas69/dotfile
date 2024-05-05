759355274d2f66c85cbbe9cfc4bd8ac1076722d9
n0xas <dusson.romain@proton.me>
2024-05-06 01:03:12 +0200

    Update zellij_fzf.sh

--- scripts/zellij_fzf.sh
+++ scripts/zellij_fzf.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/work/builds ~/projects ~/ ~/work ~/personal ~/personal/yt -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find /data ~/ -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
@@ -11,15 +11,15 @@ if [[ -z $selected ]]; then
 fi
 
 selected_name=$(basename "$selected" | tr . _)
-zellij_running=$(pgrep zellij)
+tmux_running=$(pgrep tmux)
 
-if [[ -z $zellij_running ]]; then
-    cd $selected && zellij --session $selected_name
+if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
+    tmux new-session -s $selected_name -c $selected
     exit 0
 fi
 
-# if ! tmux has-session -t=$selected_name 2> /dev/null; then
-#    tmux new-session -ds $selected_name -c $selected
-#fi
+if ! tmux has-session -t=$selected_name 2> /dev/null; then
+    tmux new-session -ds $selected_name -c $selected
+fi
 
-#tmux switch-client -t $selected_name
+tmux switch-client -t $selected_name
