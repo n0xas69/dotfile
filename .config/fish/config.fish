9fa3a6eb5e27f1a7c800ea712ee012e9a647336a
n0xas <dusson.romain@proton.me>
2024-05-07 00:24:28 +0200

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,6 @@ if status is-interactive
   starship init fish | source
   alias vim="nvim"
   set -x GOPATH /usr/local/go/bin
-  set -U fish_user_paths $fish_user_paths $GOPATH
-  eval (zellij setup --generate-auto-start fish | string collect)
+  set -x local_bin ~/.local/bin
+  set -U fish_user_paths $fish_user_paths $GOPATH $local_bin
 end
